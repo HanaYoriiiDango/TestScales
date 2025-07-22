9e89fb1d00934a459ea9ac82efc633a9ef1c1fe2
gs <gs@DESKTOP-SHPAAV5>
2025-07-22 17:07:07 +0700

    jrtjrhr

---
+++ TestScale/main.cpp
@@ -0,0 +1,11 @@
+﻿#include "Command.h"
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    srand(static_cast<unsigned int>(time(nullptr)));
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    Init_Game();
+    Start_Game();
+    
+}
